6a5f5a17214def063173b7923ac49e794814d52c
Hazem Kawas <hazem.k.4488@gmail.com>
2022-04-21 18:29:48 +0200

    add front end for signup form using mui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
 import Homepage from "./Homepage";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <ErrorBoundary>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<div>Invalid URL, sorry.</div>} />
       </Routes>
       </ErrorBoundary>
