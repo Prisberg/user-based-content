7b4af5ee55db896875f7af05a4d78ad0b82033a1
Hazem Kawas <hazem.k.4488@gmail.com>
2022-04-22 14:53:51 +0200

    add post to creat new user with hashed password

--- backEnd/models/SignUpModels.js
+++ backEnd/models/SignUpModels.js
@@ -1,25 +1,38 @@
 const mongoose = require('mongoose');
 
 const signupTemplate = new mongoose.Schema({
-  fullName:{
-    type: String,
-    required: true,
-  },
   username:{
     type: String,
-    required: true,
+    require: true,
+    min:3,
+    max:20,
+    unique: true
   },
-  email:{  type: String,
-  required: true 
+  email:{  
+    type: String,
+    require: true,
+    max:50,
+    unique: true
 },
-password: { 
-  type: String,
-  required: true,
+  password: { 
+    type: String,
+    require: true,
+    min:6,
 },
-date: {
-  type: Date,
-  default: Date.now()
-}
-})
+  profilePic: {
+    type: String,
+    default: '',
+},
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+},
+{ timestamp: true}
+)
 
 module.exports = mongoose.model('Users', signupTemplate)
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = 8800;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const routesUrls = require('./routes/routes')
-
+const registerRoutes = require('./routes/register')
+const usersRoutes = require('./routes/users')
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 dotenv.config()
-mongoose.connect(process.env.DB_ACCESS, () => console.log('DB connoted'))
+mongoose.connect(
+  process.env.DB_ACCESS,
+ 
+  () => {
+  console.log('DB connoted')
+}
+);
 
 app.use(express.json());
 app.use(cors());
-app.use("/app", routesUrls);
-// app.use("/", express.static("public"));
+
+
+app.use("/api/register", registerRoutes);
+app.use("/api/users", usersRoutes);
 
 app.listen(port, () => {console.log('app is running on port:' + port)}); 
