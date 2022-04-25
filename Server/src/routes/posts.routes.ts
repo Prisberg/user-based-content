e8c1d3b9e9212b702ba3170eb39b1b513eef634a
Hazem Kawas <hazem.k.4488@gmail.com>
2022-04-25 20:02:22 +0200

    update post

--- Server/src/routes/posts.routes.ts
+++ Server/src/routes/posts.routes.ts
@@ -4,6 +4,8 @@ import PostModel  from '../models/posts'
 
 
 const postsRouter = Router();
+
+//  Creat a New Post
 postsRouter.post('/', async (req :Request, res:Response) => {
   const newPost =  new PostModel(req.body);
   try {
@@ -13,6 +15,23 @@ postsRouter.post('/', async (req :Request, res:Response) => {
   res.status(500).json(err)
   }
 })
+
+// Edit a post
+postsRouter.put('/:id', async (req :Request, res:Response) => {
+  try { 
+    const post = await  PostModel.findById(req.params.id);
+ if (post.userId === req.body.userId) {
+   await post.updateOne({$set:req.body})
+   res.status(200).json('Post has been updated successfully');
+ } else {
+   res.status(403).json('you cant update this corse')
+ }
+ } catch (err) {
+  res.status(500).json('error: ' + err.message)
+ }
+ })
+
+
 
 
 export default postsRouter;
